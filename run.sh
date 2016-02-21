c0763c5e0d239bed202e6dd6d19ffb8a369b8788
David Anderson <davidmartinanderson@gmail.com>
2016-02-20 18:40:41 -0800

    update run command

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-#set environment and run server
+#stop running node with forever
+forever stopall
+
+#set environment and run server with forever
 NODE_ENV=production forever start /var/www/apiapp/server/server.js
