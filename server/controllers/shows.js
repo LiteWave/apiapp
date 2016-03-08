6879eb625f719965452106b8a3810277a9a4950b
tylerlandas <tylerlandas@hotmail.com>
2016-03-07 21:28:25 -0800

    Fix multiple winners in the same section AND don't calculate

--- server/controllers/shows.js
+++ server/controllers/shows.js
@@ -56,11 +56,14 @@ exports.update = function (req, res)
   var show = req.show;
   show = _.extend(show, req.body);
 
-  // Use the passed in startOffset to calculate the show start time based on server time.
-  var curTime = new Date();
-  var startTime = Math.floor(curTime.getTime() + (1000 * show.startShowOffset));
-  var startTimeDate = new Date(startTime);
-  show.startAt = startTimeDate;
+  if (show.startShowOffset === 0)
+  {
+    // Use the passed in startOffset to calculate the show start time based on server time.
+    var curTime = new Date();
+    var startTime = Math.floor(curTime.getTime() + (1000 * show.startShowOffset));
+    var startTimeDate = new Date(startTime);
+    show.startAt = startTimeDate;
+  }
 
   // Save the show
   show.save(function (err)
