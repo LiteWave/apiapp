6c2b22deb25bd04a3c34679754619654584b49eb
David Anderson <davidmartinanderson@gmail.com>
2016-09-18 20:59:10 -0700

    bug fix

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-
 . env/bin/activate
 
 # stop running node with forever
 forever stopall
 
 # set environment and run server with forever
-env = $1
+env=$1
 if [ $# -eq 0 ]
   then
-    env = "production"
+    env="production"
 fi
 
-eval NODE_ENV=$env forever start /var/www/apiapp/server/server.js
+NODE_ENV=$env forever start /var/www/apiapp/server/server.js
 
 deactivate_node
